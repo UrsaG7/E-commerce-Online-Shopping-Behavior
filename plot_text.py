6f0fce80ffc4faf1050e1447cdc9ccff12186f3f
Muhammad Gilang Mahardika <mgilangmahardika11@gmail.com>
2024-05-08 18:45:22 +0700

    Finalized, updated up to business recommendations

--- plot_text.py
+++ plot_text.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-def get_text(ax, x_horz, y_vert):
+def get_text(ax, x_horz, y_vert, font_size):
     for p in ax.patches:
         percentage = f'{p.get_height() : .2f}%'
         x = p.get_x() + p.get_width() / x_horz
         y = p.get_y() + p.get_height() + y_vert
-        ax.annotate(percentage, (x, y), size=10, ha='center', va='center')
+        ax.annotate(percentage, (x, y), size=font_size, ha='center', va='center')
 
 
 def get_text_horz(ax, x_add, n):
